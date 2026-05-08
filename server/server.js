bf7af7207c44c851c48c6a074836d3ea4233291e
premkumartech <premkumartech123@gmail.com>
2026-05-08 14:26:24 +0530

    deployment configure

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app",
+      "https://student-productivity-manager-jet.vercel.app/",
     ],
     credentials: true,
   })
