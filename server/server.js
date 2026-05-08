3bb495c83c707c05ba3df5f7a0c7b09db9b76267
premkumarmehta <codewithprem12345@gmail.com>
2026-05-08 19:51:47 +0530

    fixing api

--- server/server.js
+++ server/server.js
@@ -12,11 +12,16 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://student-productivity-manager-jet.vercel.app/api",
+      "https://student-productivity-manager-jet.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 
 // Connect DB
@@ -24,7 +29,6 @@ connectDB();
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
@@ -32,4 +36,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
